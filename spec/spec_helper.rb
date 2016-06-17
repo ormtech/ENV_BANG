a41555153e86800f2cbf7c4c8a3b3063da959a5f
Will Spurgin <will.spurgin@orm-tech.com>
2016-06-17 16:50:21 -0500

    Fix deprecation warning for SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,10 @@ require 'rspec'
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 SimpleCov.start
 
 require 'bundler/setup'
